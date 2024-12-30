36724da9c2041fa0cdd24f2b78b7549ef2aa24a2
Tom <96041552+ezTxmMC@users.noreply.github.com>
2024-12-30 15:29:39 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ API_URL="https://api.github.com/repos/SmoothCloudEU/smoothcloud/releases/latest"
 START_SCRIPT_URL="https://github.com/SmoothCloudEU/smoothcloud-manifest/raw/refs/heads/master/scripts/start.sh"
 
 # Find the URL of the launcher JAR file
-LAUNCHER_DOWNLOAD_URL=$(curl -s $API_URL | jq -r '.assets[] | select(.name | endswith(".jar")) | .browser_download_url')
+LAUNCHER_DOWNLOAD_URL=$(wget -qO- $API_URL | grep -o '"browser_download_url": "[^"]*\.jar"' | cut -d '"' -f 4)
 
 # Check if the launcher JAR URL exists
 if [ -z "$LAUNCHER_DOWNLOAD_URL" ]; then
@@ -14,12 +14,12 @@ if [ -z "$LAUNCHER_DOWNLOAD_URL" ]; then
 fi
 
 echo "Downloading the launcher file..."
-curl -L -o "smoothcloud-launcher.jar" $LAUNCHER_DOWNLOAD_URL
+wget -O "smoothcloud-launcher.jar" $LAUNCHER_DOWNLOAD_URL
 echo "Launcher file downloaded."
 
 # Download the start script and save it
 echo "Downloading the start script..."
-curl -L -o "start.sh" $START_SCRIPT_URL
+wget -O "start.sh" $START_SCRIPT_URL
 echo "Start script downloaded."
 
 rm install.sh
